a1bfd254881794a34f4552783bbd3e148154b68c
Dan Kild <dan.kild@speedandfunction.com>
2021-11-23 16:16:23 +0200

    small fixes

--- src/pages/tasks/list.js
+++ src/pages/tasks/list.js
@@ -12,11 +12,19 @@ export default function TaskList(){
     let head = ['ID', 'Task', 'Assignee', ''];
 
     function Body(){
-        return tasks.map((task) => {return (
+        
+        return tasks.map((task) => {
+            console.log(
+                '\nusers = '+users+
+                '\ntask.user_id = '+task.user_id+
+                '\nusers.length = '+users.length+
+                '\nusers[task.user_id-1].username = '+users[task.user_id-1]?.username
+                );
+            return (
             <tr>
                 <td className="align-middle">{task.id}</td>
                 <td className="align-middle"><AppLink to={"/tasks/"+task.id} text={task.task}/></td>
-                <td className="align-middle"><AppLink to={"/users/"+task.user_id} text={users[task.user_id-1].username}/></td>
+                <td className="align-middle"><AppLink to={"/users/"+task.user_id} text={users[task.user_id-1]?.username}/></td>
                 <td className="align-middle text-end"><Buttons link={'tasks/'+task.id}/></td>
             </tr>
         )})
