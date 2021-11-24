7c9e454596d3a5d4e189572e81cf4cb0fe03b2b0
Dan Kild <dan.kild@speedandfunction.com>
2021-11-24 13:44:17 +0200

    added https-redirect

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import Navbar     from "./parts/navbar";
 import Main       from "./parts/router";
 import Footer     from "./parts/footer";
 
-import HttpsRedirect from 'react-https-redirect';
 
 export default function App() {
   
   return (
       <>
-        <HttpsRedirect>
           <Navbar /> 
           <Main />
           <Footer />
-        </HttpsRedirect>
       </>
   )
 }
