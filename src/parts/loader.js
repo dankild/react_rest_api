ad23b9d88ebc48b7b8339c588b2950a7ce1114fe
Dan Kild <dan.kild@speedandfunction.com>
2021-11-24 17:50:03 +0200

    fix load spinner

--- src/parts/loader.js
+++ src/parts/loader.js
@@ -1,6 +1,7 @@
 export default function Loader(){
     return (
         <div className="d-flex align-items-center">
+            <br/><br/>
             <strong>Loading...</strong>
             <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
         </div>
